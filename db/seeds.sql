9ec601a2c3c6598f4bb2a48be587b1ba291a79e1
jessicabeauchamp <jbtroche@yahoo.com>
2016-10-09 11:23:21 -0400

    initial state files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`) VALUES ('1', 'Big Mac');
+INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`) VALUES ('2', 'Cheese Burger');
+INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`) VALUES ('3', 'Veggie Burger');
+INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`) VALUES ('4', 'Soy Burger');
+INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`) VALUES ('5', 'Black Bean Burger');
